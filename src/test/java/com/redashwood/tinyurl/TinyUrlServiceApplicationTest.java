c3208dcd026969ff83b992a773b9aa577242963d
jyotirmay-gupta <jyotirmay.gupta@outlook.com>
2025-07-19 20:34:33 +0530

    Fixed test without db connection and added badges.

--- src/test/java/com/redashwood/tinyurl/TinyUrlServiceApplicationTest.java
+++ src/test/java/com/redashwood/tinyurl/TinyUrlServiceApplicationTest.java
@@ -13,11 +13,27 @@
  */
 package com.redashwood.tinyurl;
 
+import com.redashwood.tinyurl.repository.TinyURLRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        JpaRepositoriesAutoConfiguration.class
+})
+@ActiveProfiles("test")
 class TinyUrlServiceApplicationTest {
+
+    @MockitoBean
+    private TinyURLRepository tinyURLRepository;
 
     @Test
     void contextLoads() {
