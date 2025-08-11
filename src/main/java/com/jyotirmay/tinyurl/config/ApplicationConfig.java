a4793de425e8ec2fc0ffc1cdc2ea345998a9c3d4
jyotirmay-gupta <jyotirmay.gupta@outlook.com>
2025-08-12 02:00:52 +0530

    Added filter to inject default headers.

--- src/main/java/com/jyotirmay/tinyurl/config/ApplicationConfig.java
+++ src/main/java/com/jyotirmay/tinyurl/config/ApplicationConfig.java
@@ -17,6 +17,7 @@
 
 package com.jyotirmay.tinyurl.config;
 
+import com.jyotirmay.tinyurl.filter.HeaderVersionFilter;
 import com.jyotirmay.tinyurl.repository.TinyURLRepository;
 import com.jyotirmay.tinyurl.generator.TinyURLGenerator;
 import com.jyotirmay.tinyurl.generator.impl.DistributedTinyURLGenerator;
@@ -33,6 +34,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
+
+    @Bean
+    HeaderVersionFilter registerTinyHeaderVersionFilter() {
+        return new HeaderVersionFilter();
+    }
 
     @Bean
     TinyURLGenerator configureTinyURLGenerator() {
---
+++ src/main/java/com/jyotirmay/tinyurl/filter/HeaderVersionFilter.java
@@ -0,0 +1,49 @@
+/*
+ *
+ *  * Copyright (c) 2025 Jyotirmay Gupta
+ *  *
+ *  * Project: Tiny URL
+ *  * Description: This is a personal project by Jyotirmay Gupta that implements a
+ *  * simplified URL shortening service, similar in concept to services like bit.ly or tinyurl.com.
+ *  * It allows long URLs to be converted into compact, easy-to-share short URLs.
+ *  *
+ *  * This code is intended for educational and personal use, demonstrating core backend
+ *  * concepts such as encoding algorithms, database storage, and URL mapping.
+ *  *
+ *  * Licensed under the Apache License Version 2.0. See LICENSE file for more details.
+ *
+ *
+ */
+package com.jyotirmay.tinyurl.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+
+import java.io.IOException;
+
+public class HeaderVersionFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        if (servletRequest instanceof HttpServletRequest httpRequest) {
+            HttpServletRequestWrapper wrapper = new HttpServletRequestWrapper(httpRequest) {
+                @Override
+                public String getHeader(String name) {
+                    if ("Accept-Version".equalsIgnoreCase(name) && super.getHeader(name) == null) {
+                        return "v1";
+                    }
+                    return super.getHeader(name);
+                }
+            };
+            filterChain.doFilter(wrapper, servletResponse);
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+}
